4d0e14394f8fa94f2f9ab10b1add141c2987e21b
wmoline <willowmoline@gmail.com>
2019-06-01 16:47:12 -0700

    event views

--- Club/views.py
+++ Club/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import MeetingMinutes, Meeting, Resource, Event
 
 # Create your views here.
 def index (request):
     return render(request, 'Club/index.html')
+
+def getevents(request):
+    event_list = Event.objects.all()
+    return render(request, 'Club/events.html', {'event_list': event_list})
